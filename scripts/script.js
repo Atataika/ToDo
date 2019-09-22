0d099557ee19973c8125fd8d61930a9595b232cb
Егор Бычков <egorbi2406@gmail.com>
2019-09-22 06:50:08 +0300

    remove task feature release

--- scripts/script.js
+++ scripts/script.js
@@ -2,68 +2,79 @@
 const rmAllBtn = document.querySelector(".remove-all-btn"),
     makeToDoBtn = document.querySelector(".make-to-do-btn"),
     localStorageTasks = JSON.parse(localStorage.getItem("undoneTasks"));
-let taskCounter, listItem, cacheArray;
+let listItem, cacheArray,
+    ul = document.createElement("ul");
 
-if (localStorageTasks === null) {
-    cacheArray = [];
-    taskCounter = 0;
-} else {
-    cacheArray = localStorageTasks;
-    taskCounter = cacheArray.length;
-}
+cacheArray = localStorageTasks === null ? cacheArray = [] : cacheArray = localStorageTasks;
 
-for (let i = 0; i < cacheArray.length; i++) {
+document.querySelector("section").append(ul);
+ul.addEventListener("click", listListener, false);
+makeToDoBtn.addEventListener("click", makeToDo, false);
+rmAllBtn.addEventListener("click", removeAllTasks, false);
+
+for (let key of cacheArray) {
     listItem = document.createElement("li");
-    listItem.innerHTML = localStorageTasks[i];
-    document.querySelector("ul").append(listItem);
-
-    document.getElementById(`delete-${i}`).onclick = deleteTask;
-    document.getElementById(`checkbox-${i}`).onchange = checkTask;
+    listItem.innerHTML = key;
+    ul.append(listItem);
 }
 
-makeToDoBtn.addEventListener("click", function () {
+function makeToDo() {
     const toDoInput = document.getElementById("to-do-input").value,
-        rmElemBtn = `<span id="delete-${taskCounter}">X</span>`,
-        radioInput = `<input type="checkbox" id="checkbox-${taskCounter}">`,
-        listItem = document.createElement('li');
+        listItem = document.createElement('li'),
+        radioInput = `<input type="checkbox">`,
+        rmElemBtn = `<span>X</span>`;
 
     if (toDoInput === "") {
         return alert("Input must not be empty");
     }
 
-    listItem.innerHTML = `<label>${radioInput}<span>${toDoInput}</span></label> ${rmElemBtn}`;
-    document.querySelector("ul").append(listItem);
+    listItem.innerHTML = `<label class="unchecked">${radioInput}${toDoInput}</label>${rmElemBtn}`;
 
     cacheArray.push(listItem.innerHTML);
     localStorage.setItem("undoneTasks", JSON.stringify(cacheArray));
 
-    document.getElementById(`delete-${taskCounter}`).onclick = deleteTask;
-    document.getElementById(`checkbox-${taskCounter}`).onchange = checkTask;
-
     document.getElementById("to-do-input").value = "";
-    taskCounter++;
-}, false);
-
-function deleteTask() {
-    // let a = JSON.parse(localStorage.getItem('undoneTasks'));
-    // a[???] = undefined;
-    // a.filter(key => key !== undefined);
-    this.parentElement.remove();
+    ul.append(listItem);
 }
 
-function checkTask() {
-    if (this.checked) {
-        this.parentElement.className = 'checked';
-    } else {
-        this.parentElement.className = 'unchecked';
+function listListener(event) {
+    if (event.target.nodeName === "INPUT") {
+        chengeTaskState(event);
+    } else if (event.target.nodeName === "SPAN") {
+        deleteTask(event);
     }
 }
 
-rmAllBtn.onclick = () => {
-    const ul = document.createElement("ul");
-    document.querySelector("ul").remove();
+///// Баг при удалении завершенных тасков
+///// Баг при удалении одноименных тасков
+function deleteTask(e) {
+    let tmpArray = JSON.parse(localStorage.getItem("undoneTasks"));
+
+    console.log(e);
+    for (let i = 0; i < tmpArray.length; i++) {
+        if (e.target.parentElement.innerHTML === tmpArray[i]) {
+            tmpArray[i] = undefined;
+        }
+    }
+
+    tmpArray = tmpArray.filter(key => key !== undefined);
+    localStorage.setItem("undoneTasks", JSON.stringify(tmpArray));
+    e.target.parentElement.remove();
+}
+
+function chengeTaskState(e) {
+    if (e.target.checked) {
+        e.target.parentElement.className = "checked";
+    } else {
+        e.target.parentElement.className = 'unchecked';
+    }
+}
+
+function removeAllTasks() {
+    ul.remove();
+    ul = document.createElement("ul");
     document.querySelector("section").append(ul);
-    taskCounter = 0;
+    ul.addEventListener("click", listListener, false);
     localStorage.clear();
     cacheArray = [];
 };
